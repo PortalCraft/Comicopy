fb79a711cea745e189415e3ce71e00d503a2afb1
suimisama <471899040@qq.com>
2025-02-22 19:38:02 +0800

    搜索api添加offset、platform参数

--- backend/search.py
+++ backend/search.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app, resources=r'/*')
+CORS(app, resources={r"/*": {"origins": "*"}})  # 生产环境应指定具体域名
 
 cookies = {
     'webp': '1',
@@ -23,8 +23,7 @@ headers = {
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0',
-    'cookie': 'webp=1; _ga=GA1.1.602481794.1735981222; _ga_SS4LL7K6CL=GS1.1.1740210219.12.1.1740211857.60.0.0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0'
 }
 
 
@@ -34,12 +33,14 @@ headers = {
 @app.route('/api/search', methods=['POST'])
 def search():
     # 获取表单数据
-    manga = request.form.get('manga')
-    limit = request.form.get('limit') if request.form.get('manga') is not None else 12
+    manga = request.form.get('manga', default='')
+    limit = request.form.get('limit', default=12)
+    offset = request.form.get('offset', default='0')
+    platform = request.form.get('platform', default='2')
     try:
         params = {
-            'offset': '0',
-            'platform': '2',
+            'offset': offset,
+            'platform': platform,
             'limit': limit,
             'q': manga,
             'q_type': '',
@@ -57,3 +58,4 @@ def search():
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True, port=4718)
+
