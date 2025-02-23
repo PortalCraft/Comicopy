0ccba897863d23a532c3564285016ca0416bf669
Nickyzj <1927139831@qq.com>
2025-02-23 17:39:16 +0800

    feat: 修改搜索结果为网格布局

--- frontend/app/_components/Result.tsx
+++ frontend/app/_components/Result.tsx
@@ -2,12 +2,10 @@
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Skeleton } from "@/components/ui/skeleton";
-import { cn } from "@/lib/utils";
 import { searchAtom } from "@/stores/search";
 import { useInfiniteScroll, useSize } from "ahooks";
 import { useAtom } from "jotai";
-import { Masonry, RenderComponentProps } from "masonic";
-import { CSSProperties, PropsWithChildren, useCallback, useEffect, useMemo } from "react";
+import { PropsWithChildren, useCallback, useEffect, useMemo } from "react";
 import { toast } from "sonner";
 import { SearchList, searchList } from "../_lib/api";
 import ResultItem from "./ResultItem";
@@ -19,22 +17,34 @@ type GetListResponse = {
 
 const SEARCH_LIMIT = 30;
 
-const Container = (props: PropsWithChildren<{ className?: string; style?: CSSProperties }>) => {
-  const { className, style, children } = props;
+const Container = (props: PropsWithChildren) => {
+  const { children } = props;
+
+  const bodySize = useSize(document.body);
+  const columnCount = useMemo(() => {
+    if (!bodySize) return 2;
+    const { width: bodyWidth } = bodySize;
+    if (bodyWidth > 1536) return 7;
+    if (bodyWidth > 1280) return 6;
+    if (bodyWidth > 1024) return 5;
+    if (bodyWidth > 768) return 4;
+    if (bodyWidth > 576) return 3;
+    return 2;
+  }, [bodySize]);
+
   return (
-    <div className={cn("mt-8 px-4", className)} style={style}>
+    <div
+      className="grid gap-2 px-4 mt-8"
+      style={{
+        gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`,
+      }}
+    >
       {children}
     </div>
   )
 };
 
 const List = () => {
-  const MasonryItem = useCallback(({ data }: RenderComponentProps<SearchList[number]>) => {
-    return (
-      <ResultItem {...data} />
-    )
-  }, []);
-
   /**
    * 搜索相关逻辑
    */
@@ -92,26 +102,9 @@ const List = () => {
 
   const items = useMemo(() => data?.list ?? [], [data]);
 
-  const bodySize = useSize(document.body);
-  const columnCount = useMemo(() => {
-    if (!bodySize) return 2;
-    const { width: bodyWidth } = bodySize;
-    if (bodyWidth > 1536) return 7;
-    if (bodyWidth > 1280) return 6;
-    if (bodyWidth > 1024) return 5;
-    if (bodyWidth > 768) return 4;
-    if (bodyWidth > 576) return 3;
-    return 2;
-  }, [bodySize]);
-
   if (loading) {
     return (
-      <Container
-        className="grid gap-2"
-        style={{
-          gridTemplateColumns: `repeat(${columnCount}, minmax(0, 1fr))`,
-        }}
-      >
+      <Container>
         {Array.from({ length: SEARCH_LIMIT }).map((_, index) => (
           <AspectRatio key={index} ratio={350 / 500}>
             <Skeleton className="size-full rounded-xl" />
@@ -123,13 +116,9 @@ const List = () => {
 
   return (
     <Container>
-      <Masonry
-        items={items}
-        render={MasonryItem}
-        rowGutter={8}
-        columnGutter={8}
-        columnCount={columnCount}
-      />
+      {items.map((item) => (
+        <ResultItem key={item.id} {...item} />
+      ))}
     </Container>
   );
 };
