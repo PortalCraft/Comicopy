825646a493ca8b809f9b031162ac207239374f0a
Nickyzj <1927139831@qq.com>
2025-03-02 15:21:17 +0800

    chore: 完善 bat 脚本

--- install_deps.bat
+++ install_deps.bat
@@ -3,19 +3,25 @@
 :: 脚本所在目录
 set SCRIPT_DIR=%~dp0
 
+:: 安装前端依赖
+echo Install frontend dependencies...
+cd %SCRIPT_DIR%/frontend
+call pnpm install
+if %errorlevel% neq 0 (
+    echo Failed to install frontend dependencies
+    exit /b %errorlevel%
+)
+
 :: 安装后端 Python 依赖
 echo Install backend python dependencies...
+cd %SCRIPT_DIR%/backend
 pip install -r requirements.txt
 if %errorlevel% neq 0 (
     echo Failed to install backend dependencies
     exit /b %errorlevel%
 )
 
-:: 安装前端依赖
-echo Install frontend dependencies...
-cd %SCRIPT_DIR%/frontend
-pnpm install
-if %errorlevel% neq 0 (
-    echo Failed to install frontend dependencies
-    exit /b %errorlevel%
-)
+:: 回到脚本所在目录
+cd %SCRIPT_DIR%/
+echo Installation successfully!
+pause
